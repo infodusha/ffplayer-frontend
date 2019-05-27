2040d4891dcfcd3fc10c74c7278907054b8442e8
infodusha <infodusha@ya.ru>
2019-05-27 12:51:55 +0300

    Ineria in useSwipeArea

--- hooks/useSwipeArea.js
+++ hooks/useSwipeArea.js
@@ -9,12 +9,35 @@ export function useSwipeArea(ref, length, setScroll) {
 
         let startX;
         let dtX;
+        let startT;
+        let interval;
+
+        function inertia(v0) {
+            let s = 10;
+            let a = v0 / 1000;
+            let v = (t) => v0 + a*t;
+            let time = Date.now();
+            interval = window.setInterval(() => {
+                let t = Date.now() - time;
+                if(t >= Math.abs(v0) * 100) {
+                    window.clearInterval(interval);
+                    interval = null;
+                    return;
+                }
+                setScroll((scroll) => Math.max(0, Math.min(scroll + s * v(t), length)));
+            }, 10);
+        }
 
         function start({ touches }) {
             if(touches.length !== 1)
                 return;
+            if(interval) {
+                window.clearInterval(interval);
+                interval = null;
+            }
             dtX = 0;
             startX = touches[0].pageX;
+            startT = Date.now();
         }
 
         function move({ touches }) {
@@ -25,12 +48,22 @@ export function useSwipeArea(ref, length, setScroll) {
             dtX = dtScroll;
         }
 
+        function end({ touches }) {
+            if(touches.length !== 0)
+                return;
+            let dtT = Date.now() - startT;
+            let dtV = dtX / dtT;
+            inertia(dtV);
+        }
+
 
         item.addEventListener('touchstart', start);
         item.addEventListener('touchmove', move);
+        item.addEventListener('touchend', end);
         return () => {
             item.removeEventListener('touchstart', start);
             item.removeEventListener('touchmove', move);
+            item.removeEventListener('touchend', end);
         };
     }, [length, ref, setScroll]);
 
