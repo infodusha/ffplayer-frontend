35d5f739981d14ae2f9a856ec8977af95f917232
infodusha <infodusha@ya.ru>
2019-08-22 20:52:35 +0300

    UI: Modal - fix overcode for scroll fix effect

--- ui/modal/index.js
+++ ui/modal/index.js
@@ -1,39 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function Modal({ open = false, children }) {
 
-    let scroll = useRef(null);
-
-    useEffect(() => {
+    useLayoutEffect(() => {
         if(open) {
-            document.body.style.position = 'fixed';
-            document.body.style.top = `-${scroll.current}px`;
-            document.body.style.right = '0px';
-            document.body.style.left = '0px';
+            document.body.style.overflow = 'hidden';
             return () => {
-                const scrollY = document.body.style.top;
-                document.body.style.position = '';
-                document.body.style.top = '';
-                document.body.style.right = '';
-                document.body.style.left = '';
-                window.scrollTo(0, parseInt(scrollY || '0') * -1);
+                document.body.style.overflow = '';
             };
         }
-    }, [open]);
-
-    useEffect(() => {
-        function handleScroll() {
-            scroll.current = window.pageYOffset || window.document.documentElement.scrollTop;
-        }
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    useEffect(() => {
-        if(scroll.current === null && open)
-            throw new Error('Modal should be closed on first render');
-        scroll.current = window.pageYOffset || window.document.documentElement.scrollTop;
     }, [open]);
 
     if(!open)
